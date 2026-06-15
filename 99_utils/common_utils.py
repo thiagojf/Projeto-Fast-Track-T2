208fc02f0b9ead6d50f045f0d344ef679dbdd1ff
Thiago Faria <thiago_faria@outlook.com>
2026-06-15 16:24:40 -0300

    Arquivo que centraliza o carregamento de todos os utilitarios no notebooks.

---
+++ 99_utils/common_utils.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+# MAGIC %run /Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/Desafio_Final_Compass_V2.1/99_Utils/api_utils
+
+# COMMAND ----------
+
+# MAGIC %run /Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/Desafio_Final_Compass_V2.1/99_Utils/audit_utils
+
+# COMMAND ----------
+
+# MAGIC %run /Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/Desafio_Final_Compass_V2.1/99_Utils/delta_utils
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %run /Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/Desafio_Final_Compass_V2.1/99_Utils/log_utils
