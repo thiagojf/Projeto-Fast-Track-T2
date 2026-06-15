eb99188769be65c733c59f8ddb70600d4ee5e8a7
Thiago Faria <thiago_faria@outlook.com>
2026-06-15 16:22:07 -0300

    Padroniza a geração de logs de execução, avisos e erros durante os pipelines.

---
+++ 99_utils/log_utils.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+def log_info(msg):
+
+    print(f"[INFO] {msg}")
+
+def log_warning(msg):
+
+    print(f"[WARNING] {msg}")
+
+def log_error(msg):
+
+    print(f"[ERROR] {msg}")
