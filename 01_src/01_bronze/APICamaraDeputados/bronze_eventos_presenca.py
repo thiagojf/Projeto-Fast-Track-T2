f9d534e3a2f97f7c3fe30f378521a3d66366df2a
Thiago Faria <thiago_faria@outlook.com>
2026-06-15 20:31:50 -0300

    Refatorado o notebook, a função request com retry foi removida do arquivo devido a ter sido criado no api_utils. No item INGESTÃO COM PAGINAÇÃO A carga passou a realizar gravação incremental página a página, reduzindo o risco de perda de dados em caso de falha durante a ingestão.

--- 01_src/01_bronze/APICamaraDeputados/bronze_eventos_presenca.py
+++ 01_src/01_bronze/APICamaraDeputados/bronze_eventos_presenca.py
@@ -9,17 +9,15 @@
 # Endpoint: /eventos/{id}/deputados
 # ============================================================
 
-import requests
-import time
-import uuid
-import json
-
-from pyspark.sql import functions as F
-
-
 # ============================================================
 # 1. CONFIGURAÇÕES
 # ============================================================
+
+import json
+import time
+import uuid
+
+from pyspark.sql import functions as F
 
 BASE_URL = "https://dadosabertos.camara.leg.br/api/v2"
 
@@ -34,135 +32,158 @@ PIPELINE_VERSION = "1.0"
 BATCH_ID = str(uuid.uuid4())
 
 
-
 # ============================================================
-# 3. OBTER IDS DOS EVENTOS
+# 2. OBTÉM IDS DOS EVENTOS
 # ============================================================
 
 ids_eventos = [
+
     row["id"]
+
     for row in (
         spark.table(TABELA_ORIGEM_EVENTOS)
         .select("id")
         .distinct()
-        #.limit(5)  # teste inicial; remova ou aumente depois
         .collect()
     )
 ]
 
-print(f"[INFO] Total de eventos para processar: {len(ids_eventos)}")
+total_eventos = len(ids_eventos)
+
+log_info(
+    f"Total de eventos para processar: {total_eventos}"
+)
 
 
 # ============================================================
-# 4. INGESTÃO DE PRESENÇA POR EVENTO
+# 3. CONTROLE DE ERROS
 # ============================================================
 
-lista_presenca = []
 lista_erros = []
-pagina = 1
 
-while True:
+
+# ============================================================
+# 4. PROCESSAMENTO DOS EVENTOS
+# ============================================================
+
+for contador, id_evento in enumerate(ids_eventos, start=1):
 
     try:
-        for id_evento in ids_eventos:
 
-            endpoint_atual = f"/eventos/{id_evento}/deputados"
-            url_atual = f"{BASE_URL}{endpoint_atual}"
+        endpoint_atual = f"/eventos/{id_evento}/deputados"
 
-            log_info(
-            f"[INFO] Coletando presença do evento {id_evento}"
-            )
-            # ============================================
-            # REQUEST API
-            # ============================================
+        url_atual = (
+            f"{BASE_URL}{endpoint_atual}"
+        )
 
-            try:
-                response_json = get_api_data(url=url_atual)
+        log_info(
+            f"Processando evento "
+            f"{contador}/{total_eventos} "
+            f"- ID {id_evento}"
+        )
 
-            except Exception as e:
-                print(f"[ERROR] Falha definitiva evento {id_evento}: {str(e)}")
-
-                lista_erros.append({
-                    "id_evento": id_evento,
-                    "endpoint": endpoint_atual,
-                    "erro": str(e)
-                })
-
-                continue
-            # ============================================
-            # DADOS
-            # ============================================
-            dados = response_json.get("dados", [])
-            # ============================================
-            # CONDIÇÃO DE PARADA
-            # ============================================
-            if not dados:
-                log_info(
-               "[WARNING] Nenhum deputado retornado para evento {id_evento}"
-               )
-                continue
-            # ============================================
-            # RAW PAYLOAD
-            # ============================================
-            for deputado in dados:
-
-                deputado["id_evento"] = id_evento
-                deputado["source_endpoint_detail"] = endpoint_atual
-                deputado["raw_payload"] = json.dumps(deputado, ensure_ascii=False)
-
-                lista_presenca.append(deputado)
-
-            print(
-                f"[INFO] Evento {id_evento} | Deputados: {len(dados)} | "
-                f"Acumulado: {len(lista_presenca)}"
-            )
-
-            time.sleep(0.2)
         # ============================================
-        # CRIAÇÃO DO DATAFRAME
+        # REQUEST API
         # ============================================
 
-            if not lista_presenca:
-                raise Exception("Nenhuma presença retornada pela API.")
+        response_json = get_api_data(
+            url=url_atual,
+            max_retries=MAX_RETRIES,
+            retry_delay=RETRY_DELAY,
+            timeout=TIMEOUT
+        )
 
-            json_strings = [
-                json.dumps(registro, ensure_ascii=False)
-                for registro in lista_presenca
-            ]
+        dados = response_json.get(
+            "dados",
+            []
+        )
 
-            spark_df_raw = spark.createDataFrame(
-                [(item,) for item in json_strings],
-                ["json_string"]
+        # ============================================
+        # EVENTO SEM PRESENÇA
+        # ============================================
+
+        if not dados:
+
+            log_warning(
+                f"Evento {id_evento} sem presença."
             )
 
-            schema_presenca = """
-            struct<
-                id:bigint,
-                uri:string,
-                nome:string,
-                siglaPartido:string,
-                uriPartido:string,
-                siglaUf:string,
-                idLegislatura:bigint,
-                urlFoto:string,
-                email:string,
-                id_evento:bigint,
-                source_endpoint_detail:string,
-                raw_payload:string
-            >
-            """
+            continue
 
-            spark_df = (
-                spark_df_raw
-                .select(
-                    F.from_json(
-                        F.col("json_string"),
-                        schema_presenca
-                    ).alias("dados")
-                )
-                .select("dados.*")
+        # ============================================
+        # PREPARAÇÃO DOS DADOS
+        # ============================================
+
+        lista_presenca = []
+
+        for deputado in dados:
+
+            deputado["id_evento"] = id_evento
+
+            deputado[
+                "source_endpoint_detail"
+            ] = endpoint_atual
+
+            deputado["raw_payload"] = json.dumps(
+                deputado,
+                ensure_ascii=False
             )
 
+            lista_presenca.append(
+                deputado
+            )
+
+        # ============================================
+        # JSON -> DATAFRAME
+        # ============================================
+
+        json_strings = [
+
+            json.dumps(
+                registro,
+                ensure_ascii=False
+            )
+
+            for registro in lista_presenca
+
+        ]
+
+        spark_df_raw = spark.createDataFrame(
+            [(item,) for item in json_strings],
+            ["json_string"]
+        )
+
+        schema_presenca = """
+        struct<
+            id:bigint,
+            uri:string,
+            nome:string,
+            siglaPartido:string,
+            uriPartido:string,
+            siglaUf:string,
+            idLegislatura:bigint,
+            urlFoto:string,
+            email:string,
+            id_evento:bigint,
+            source_endpoint_detail:string,
+            raw_payload:string
+        >
+        """
+
+        spark_df = (
+
+            spark_df_raw
+
+            .select(
+                F.from_json(
+                    F.col("json_string"),
+                    schema_presenca
+                ).alias("dados")
+            )
+
+            .select("dados.*")
+
+        )
 
         # ============================================
         # AUDITORIA
@@ -175,16 +196,24 @@ while True:
             pipeline_version=PIPELINE_VERSION
         )
 
-       # ============================================
-        # ESCRITA DELTA
-        # Primeira execução = overwrite
-        # Próximas execuções = append
         # ============================================
-        if not spark.catalog.tableExists(TABELA_DESTINO):
+        # DEFINIÇÃO MODO DE ESCRITA
+        # ============================================
+
+        if not spark.catalog.tableExists(
+            TABELA_DESTINO
+        ):
+
             modo_escrita = "overwrite"
+
         else:
+
             modo_escrita = "append"
-        
+
+        # ============================================
+        # ESCRITA DELTA
+        # ============================================
+
         salvar_delta(
             df=spark_df,
             tabela=TABELA_DESTINO,
@@ -197,29 +226,48 @@ while True:
         )
 
         log_info(
-            f"Página {pagina} gravada com sucesso."
+            f"Evento {id_evento} gravado com sucesso."
         )
-
-        pagina += 1
-
-        # ============================================
-        # RATE LIMIT
-        # ============================================
 
         time.sleep(0.2)
 
     except Exception as e:
 
         log_error(
-            f"Erro na página {pagina}: {str(e)}"
+            f"Erro no evento {id_evento}: {str(e)}"
         )
 
-        raise
-        
+        lista_erros.append({
+
+            "id_evento": id_evento,
+
+            "endpoint": endpoint_atual,
+
+            "erro": str(e)
+
+        })
+
+        continue
+
 
 # ============================================================
-# 4. FINALIZAÇÃO
+# 5. RESUMO EXECUÇÃO
 # ============================================================
+
+log_info(
+    f"Processamento concluído. "
+    f"Eventos com erro: {len(lista_erros)}"
+)
+
+if lista_erros:
+
+    log_warning(
+        "Eventos com falha durante a execução:"
+    )
+
+    for erro in lista_erros:
+
+        print(erro)
 
 log_info(
     "Ingestão concluída com sucesso."
