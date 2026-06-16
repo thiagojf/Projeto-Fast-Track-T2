7d08f63d57418931aa00c271c82a6599bf0d842f
Thiago Faria <thiago_faria@outlook.com>
2026-06-16 16:36:43 -0300

    Adicionado o comando para carregar as configuracoes globais e funcoes utilitarias.

--- 01_src/02_silver/silver_dim_deputados.py
+++ 01_src/02_silver/silver_dim_deputados.py
@@ -1,3 +1,8 @@
+# Databricks notebook source
+# MAGIC %run /Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/Desafio_Final_Compass_V2.1/99_Utils/common_utils
+
+# COMMAND ----------
+
 # ============================================================
 # SILVER - DIM_DEPUTADO
 # ============================================================
