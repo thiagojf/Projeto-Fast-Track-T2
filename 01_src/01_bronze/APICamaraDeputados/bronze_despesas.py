7e1974c9402ccc51d36c6fd48f37e8ac00fe6c17
Thiago Faria <thiago_faria@outlook.com>
2026-06-15 21:29:31 -0300

    Inicialmente o notebook acumulava todas as despesas em memória e gravava apenas ao final da execução, então alterei a estratégia para persistência incremental por deputado. Dessa forma, cada deputado processado já é gravado na Bronze, reduzindo risco de perda de dados em caso de falha e aumentando a resiliência do pipeline.

--- 01_src/01_bronze/APICamaraDeputados/bronze_despesas.py
+++ 01_src/01_bronze/APICamaraDeputados/bronze_despesas.py
@@ -4,7 +4,7 @@
 # COMMAND ----------
 
 # ============================================================
-# BRONZE_DESPESAS
+# BRONZE DESPESAS POR DEPUTADO
 # Camada Bronze | Ingestão RAW API Câmara
 # ============================================================
 
@@ -37,18 +37,17 @@ TABELA_ORIGEM_DEPUTADOS = "desafio_final_t2.bronze.bronze_deputados"
 TABELA_DESTINO = "desafio_final_T2.bronze.bronze_despesas"
 
 # ============================================================
-# 2. OBTER IDS DAS FRENTES JÁ INGESTADAS
+# 2. OBTER IDS DOS DEPUTADOS
 # ============================================================
 
 #ids_deputados = [204379]
-
 ids_deputados = [
     row["id"]
     for row in (
         spark.table(TABELA_ORIGEM_DEPUTADOS)
         .select("id")
         .distinct()
-        .limit(5)
+        #.limit(5)
         .collect()
     )
 ]
@@ -57,21 +56,40 @@ print(f"[INFO] Total de deputados para processar: {len(ids_deputados)}")
 
 
 # ============================================================
-# 3. INGESTÃO DOS MEMBROS POR FRENTE
+# 3. INGESTÃO DAS DESPESAS POR DEPUTADO
 # ============================================================
 
-lista_despesas = []
-
 try:
-    log_info("Iniciando ingestão de despesas")
+
+    log_info(
+        "Iniciando ingestão de despesas"
+    )
+    # ================ 15/06/2026 - Thiago Faria =========
+    # Verifica se a tabela já existe na primeira execução não existindo overwrite (cria a tabela).
+    # Demais execuções usa append para evitar duplicidade
+    # ====================================================
+    primeira_carga = not spark.catalog.tableExists(
+        TABELA_DESTINO
+    )
 
     for id_deputado in ids_deputados:
+        # ========= 15/06/2026 - Thiago Faria =========
+        # Lista exclusiva do deputado atual quando terminar o processamento deste deputado os dados já serão gravados.
+        # Se ocorrer erro no próximo deputado, os anteriores já estarão persistidos.
+        # ============================================
+        lista_despesas_deputado = []
 
         for ano_referencia in ANOS_REFERENCIA:
 
             pagina = 1
-            endpoint_atual = f"/deputados/{id_deputado}/despesas"
-            url_atual = f"{BASE_URL}{endpoint_atual}"
+
+            endpoint_atual = (
+                f"/deputados/{id_deputado}/despesas"
+            )
+
+            url_atual = (
+                f"{BASE_URL}{endpoint_atual}"
+            )
 
             while True:
 
@@ -81,14 +99,16 @@ try:
                     "itens": ITENS_POR_PAGINA
                 }
 
-                print(
-                    f"[INFO] Deputado {id_deputado} | "
-                    f"Ano {ano_referencia} | Página {pagina}"
+                log_info(
+                    f"Deputado {id_deputado} | "
+                    f"Ano {ano_referencia} | "
+                    f"Página {pagina}"
                 )
 
                 # ============================================
                 # REQUEST API
                 # ============================================
+
                 response_json = get_api_data(
                     url=url_atual,
                     params=params
@@ -97,126 +117,194 @@ try:
                 # ============================================
                 # DADOS
                 # ============================================
-                dados = response_json.get("dados", [])
+
+                dados = response_json.get(
+                    "dados",
+                    []
+                )
 
                 # ============================================
-                # CONDIÇÃO DE PARADA
+                # FIM PAGINAÇÃO
                 # ============================================
+
                 if not dados:
-                    print(
-                        f"[INFO] Fim deputado {id_deputado} | "
+
+                    log_info(
+                        f"Fim deputado {id_deputado} | "
                         f"Ano {ano_referencia}"
                     )
+
                     break
 
                 # ============================================
                 # RAW PAYLOAD
                 # ============================================
+
                 for despesa in dados:
+
                     despesa["nk_deputado"] = id_deputado
-                    despesa["ano_referencia"] = ano_referencia
-                    despesa["source_endpoint_detail"] = endpoint_atual
+
+                    despesa["ano_referencia"] = (
+                        ano_referencia
+                    )
+
+                    despesa[
+                        "source_endpoint_detail"
+                    ] = endpoint_atual
+
                     despesa["raw_payload"] = json.dumps(
                         despesa,
                         ensure_ascii=False
                     )
-                    lista_despesas.append(despesa)
+                    # ======== 15/06/2026 - Thiago Faria =========
+                    # Armazena somente despesas do deputado atual, lista criada no inicio
+                    # ============================================
+                    lista_despesas_deputado.append(
+                        despesa
+                    )
 
-                print(f"[INFO] Registros acumulados: {len(lista_despesas)}")
+                log_info(
+                    f"Registros acumulados deputado "
+                    f"{id_deputado}: "
+                    f"{len(lista_despesas_deputado)}"
+                )
 
                 pagina += 1
+
                 time.sleep(0.2)
 
-    # ============================================================
-    # CRIAÇÃO DO DATAFRAME
-    # ============================================================
-    if not lista_despesas:
-        raise Exception(
-            f"Nenhuma despesa retornada pela API para os anos {ANOS_REFERENCIA}."
+        # ================ 15/06/2026 - Thiago Faria =========
+        # Valida a lista de despesas por dputado e retorna se temos dados para persistir não encontrano retorna um log_warning mas continua o processo
+        # para processar as despeas do proximo deputado
+        # ====================================================
+
+        if not lista_despesas_deputado:
+
+            log_warning(
+                f"Nenhuma despesa encontrada "
+                f"para deputado {id_deputado}"
+            )
+
+            continue
+
+        # ====================================================
+        # CRIAÇÃO DATAFRAME
+        # ====================================================
+
+        json_strings = [
+
+            json.dumps(
+                registro,
+                ensure_ascii=False
+            )
+
+            for registro in lista_despesas_deputado
+        ]
+
+        spark_df_raw = spark.createDataFrame(
+            [(item,) for item in json_strings],
+            ["json_string"]
         )
 
-    json_strings = [
-        json.dumps(registro, ensure_ascii=False)
-        for registro in lista_despesas
-    ]
+        schema_despesas = """
+        struct<
+            ano:int,
+            mes:int,
+            tipoDespesa:string,
+            codDocumento:bigint,
+            tipoDocumento:string,
+            codTipoDocumento:int,
+            dataDocumento:string,
+            numDocumento:string,
+            valorDocumento:double,
+            urlDocumento:string,
+            nomeFornecedor:string,
+            cnpjCpfFornecedor:string,
+            valorLiquido:double,
+            valorGlosa:double,
+            numRessarcimento:string,
+            codLote:bigint,
+            parcela:int,
+            nk_deputado:bigint,
+            ano_referencia:int,
+            source_endpoint_detail:string,
+            raw_payload:string
+        >
+        """
 
-    spark_df_raw = spark.createDataFrame(
-        [(item,) for item in json_strings],
-        ["json_string"]
-    )
+        spark_df = (
 
-    schema_despesas = """
-    struct<
-        ano:int,
-        mes:int,
-        tipoDespesa:string,
-        codDocumento:bigint,
-        tipoDocumento:string,
-        codTipoDocumento:int,
-        dataDocumento:string,
-        numDocumento:string,
-        valorDocumento:double,
-        urlDocumento:string,
-        nomeFornecedor:string,
-        cnpjCpfFornecedor:string,
-        valorLiquido:double,
-        valorGlosa:double,
-        numRessarcimento:string,
-        codLote:bigint,
-        parcela:int,
-        nk_deputado:bigint,
-        ano_referencia:int,
-        source_endpoint_detail:string,
-        raw_payload:string
-    >
-    """
+            spark_df_raw
 
-    spark_df = (
-        spark_df_raw
-        .select(F.from_json(F.col("json_string"), schema_despesas).alias("dados"))
-        .select("dados.*")
-    )
+            .select(
 
-    # ============================================
-    # AUDITORIA
-    # ============================================
-    spark_df = adicionar_auditoria(
-        df=spark_df,
-        endpoint=ENDPOINT_BASE,
-        batch_id=BATCH_ID,
-        pipeline_version=PIPELINE_VERSION
-    )
+                F.from_json(
+                    F.col("json_string"),
+                    schema_despesas
+                ).alias("dados")
 
-    # ============================================
-    # ESCRITA DELTA
-    # Primeira execução = overwrite
-    # Próximas execuções = append
-    # ============================================
-    if not spark.catalog.tableExists(TABELA_DESTINO):
-        modo_escrita = "overwrite"
-    else:
-        modo_escrita = "append"
+            )
 
-    salvar_delta(
-        df=spark_df,
-        tabela=TABELA_DESTINO,
-        modo=modo_escrita,
-        particionar=True,
-        colunas_particao=[
-            "ano_ingestao",
-            "mes_ingestao"
-        ]
-    )
+            .select("dados.*")
 
-    log_info(
-        f"{len(lista_despesas)} registros gravados com sucesso."
-    )
+        )
+
+        # ====================================================
+        # AUDITORIA
+        # ====================================================
+
+        spark_df = adicionar_auditoria(
+            df=spark_df,
+            endpoint=ENDPOINT_BASE,
+            batch_id=BATCH_ID,
+            pipeline_version=PIPELINE_VERSION
+        )
+
+        # ========= 15/06/2026 - Thiago Faria =========
+        # ESCRITA DELTA
+        # Na p1rimeira execução vai gravar no modo overwrite
+        # Nas demais gravações grava no modo append
+        # Dessa forma conseguimos gravar deputado por deputado sem recriar a tabela.
+        # ====================================================
+        modo_escrita = (
+            "overwrite"
+            if primeira_carga
+            else "append"
+        )
+
+        salvar_delta(
+            df=spark_df,
+            tabela=TABELA_DESTINO,
+            modo=modo_escrita,
+            particionar=True,
+            colunas_particao=[
+                "ano_ingestao",
+                "mes_ingestao"
+            ]
+        )
+        # ================ 15/06/2026 - Thiago Faria =========
+        # Após a primeira gravação a tabela já existe e a varivael primeira_carga passa aser FALSE e as próximas gravações serão append.
+        # ====================================================
+        primeira_carga = False
+
+        log_info(
+            f"Deputado {id_deputado} gravado com "
+            f"{len(lista_despesas_deputado)} registros."
+        )
 
 except Exception as e:
-    log_error(f"Erro durante ingestão: {str(e)}")
+
+    log_error(
+        f"Erro durante ingestão: {str(e)}"
+    )
+
     raise
+
 
 # ============================================================
 # 4. FINALIZAÇÃO
 # ============================================================
-log_info("Ingestão concluída com sucesso.")
+
+log_info(
+    "Ingestão concluída com sucesso."
+)
