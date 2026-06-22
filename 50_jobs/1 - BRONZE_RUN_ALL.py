c71e670c02c1b69c29c48de6ade8d6a6a0503a24
thiago_faria@outlook.com <thiago_faria@outlook.com>
2026-06-22 01:50:37 +0000

    feat(50_jobs/1 - BRONZE_RUN_ALL.py): Ajustado nome dos notebooks

--- 50_jobs/1 - BRONZE_RUN_ALL.py
+++ 50_jobs/1 - BRONZE_RUN_ALL.py
@@ -1,13 +1,16 @@
+
+
 notebooks = [
     "/Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/GIT_Projeto-Fast-Track-T2/01_src/01_bronze/APICamaraDeputados/bronze_deputados",
-    "/Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/GIT_Projeto-Fast-Track-T2/01_src/01_bronze/APICamaraDeputados/bronze_frentes",
+    "/Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/GIT_Projeto-Fast-Track-T2/01_src/01_bronze/APICamaraDeputados/bronze_Frentes",
     "/Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/GIT_Projeto-Fast-Track-T2/01_src/01_bronze/APICamaraDeputados/bronze_frentes_membros",
     "/Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/GIT_Projeto-Fast-Track-T2/01_src/01_bronze/APICamaraDeputados/bronze_eventos",
-    "/Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/GIT_Projeto-Fast-Track-T2/01_src/01_bronze/APICamaraDeputados/bronze_eventos_presenca",    
+    "/Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/GIT_Projeto-Fast-Track-T2/01_src/01_bronze/APICamaraDeputados/bronze_eventos_presenca",       
     "/Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/GIT_Projeto-Fast-Track-T2/01_src/01_bronze/APICamaraDeputados/bronze_tipo_evento",    
     "/Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/GIT_Projeto-Fast-Track-T2/01_src/01_bronze/APICamaraDeputados/bronze_despesas",
     "/Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/GIT_Projeto-Fast-Track-T2/01_src/01_bronze/APICamaraDeputados/bronze_orgao",
     "/Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/GIT_Projeto-Fast-Track-T2/01_src/01_bronze/APICamaraDeputados/bronze_orgaos_detalhe",
+
     "/Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/GIT_Projeto-Fast-Track-T2/01_src/01_bronze/APICamaraDeputados/bronze_votacoes",
     "/Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/GIT_Projeto-Fast-Track-T2/01_src/01_bronze/APICamaraDeputados/bronze_votacoes_votos"
 
