42c0ed2b902f64649c42f93ca3a768aeeaf7f0ea
thiago_faria@outlook.com <thiago_faria@outlook.com>
2026-06-23 21:27:11 +0000

    docs(common_utils): Inserido titulo nas celulas para comentario.

--- 99_utils/common_utils.py
+++ 99_utils/common_utils.py
@@ -1,15 +1,19 @@
 # Databricks notebook source
+# DBTITLE 1,Utilitário para consumo de APIs externas
 # MAGIC %run /Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/GIT_Projeto-Fast-Track-T2/99_utils/api_utils
 
 # COMMAND ----------
 
+# DBTITLE 1,Utilitário para auditoria e rastreabilidade
 # MAGIC %run /Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/GIT_Projeto-Fast-Track-T2/99_utils/audit_utils
 
 # COMMAND ----------
 
+# DBTITLE 1,Utilitário para carregamento das funções Delta Lake
 # MAGIC %run /Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/GIT_Projeto-Fast-Track-T2/99_utils/delta_utils
 # MAGIC
 
 # COMMAND ----------
 
+# DBTITLE 1,Utilitário para carregamento das funções de log
 # MAGIC %run /Workspace/Users/thiagofaria87@escoladotrabalhador40.com.br/GIT_Projeto-Fast-Track-T2/99_utils/log_utils
